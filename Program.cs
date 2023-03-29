8c78b47064b6d14a98caeb1e33f3c0d6b4596544
GrandeCapo22 <fabriziozampetti05@gmail.com>
2023-03-29 17:07:50 +0200

    Lettura singolo giocatore
    StreamReader per leggere ogni giocatore presente nel file chiamato "DB_Giocatori" su bin, debug.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GestioneCampionato
 {
@@ -22,8 +23,24 @@ namespace GestioneCampionato
 				delegate(){ Console.WriteLine("metodo 7"); },
 				delegate(){ Console.WriteLine("metodo 8"); },
 			};
-			Graphic.Switcher("menumolto lungo", 0, 0, new string[] { "partita", "campionato", "giocatori", "classifica", "giocatori", "arbitri", "allenatori", "exit" }, metodi, ref index, 0, 1, true, true);
-			new Persona("fabrizio", "gay", "fbrgay10p05b114m", DateTime.Now).Visualizza();
+			//Graphic.Switcher("menumolto lungo", 0, 0, new string[] { "partita", "campionato", "giocatori", "classifica", "giocatori", "arbitri", "allenatori", "exit" }, metodi, ref index, 0, 1, true, true);
+			//new Persona("fabrizio", "gay", "fbrgay10p05b114m", DateTime.Now).Visualizza();
+			//Console.ReadKey();
+
+			StreamReader SR = new StreamReader("DB_Giocatori.txt");
+			String[] Giocatori = new String[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+				Giocatori[i] = SR.ReadLine();
+            }
+			Console.ReadKey();
+
+			String[] pl1 = Giocatori[0].Split(',');
+            for (int i = 0; i < pl1.Length; i++)
+            {
+				Console.WriteLine(pl1[i]);
+            }
 			Console.ReadKey();
 		}
 	}
