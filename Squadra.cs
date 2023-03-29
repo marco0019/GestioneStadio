ee19fe8285a1be15846c02ea483e693bbebc9c5e
GrandeCapo22 <fabriziozampetti05@gmail.com>
2023-03-29 16:04:16 +0200

    Classe Squadra: metodi costruttore, inserimento, visualizza, sostituisci, elimina

--- Squadra.cs
+++ Squadra.cs
@@ -16,15 +16,84 @@ namespace GestioneCampionato
 		public Persona[] Giocatori { get => giocatori; set => giocatori = value; }
 		public Persona Allenatore { get => allenatore; set=> allenatore = value; }
 		public Persona Presidente { get => presidente; set => presidente = value; }
-		public string Nome { get => nome; }
+		public string Nome { get => nome;}
 		public string Stadio { get => stadio; }
-		public Squadra()
+		public Squadra(String _nome, Persona _presidente, String _stadio, Persona[] _giocatori)
 		{
-
+			this.nome = _nome[0].ToString().ToUpper() + _nome.Substring(1).ToString().ToLower();
+			this.Presidente = _presidente;
+			this.stadio = _stadio;
+			this.Giocatori = _giocatori;
+			this.NumeroGiocatori++;
 		}
+
+		public void Inserimento(Persona _persona, String _ruolo = "giocatore")
+        {
+            switch (_ruolo.ToLower())
+            {
+				case "giocatore":
+					this.Giocatori[this.NumeroGiocatori] = _persona;
+					this.NumeroGiocatori++;
+					break;
+				case "presidente":
+					this.Presidente = _persona;
+					break;
+				case "allenatore":
+					this.Allenatore = _persona;
+					break;
+			}
+        }
+
 		public void Visualizza()
 		{
+			Console.WriteLine(this.Nome);
+			Console.WriteLine("Presidente: ");
+			this.Presidente.Visualizza();
+			Console.WriteLine("Allenatore: ");
+			this.Allenatore.Visualizza();
+			Console.WriteLine("Stadio: " + this.Stadio);
+			Console.WriteLine();
+			Console.WriteLine("Rosa: ");
+            for (int i = 0; i < this.NumeroGiocatori; i++)
+            {
+				Console.Write(this.Giocatori[i].Cognome + " " + this.Giocatori[i].Nome);
+				if (i % 2 == 0) Console.WriteLine(); 
+            }
+		}
 
+		public void Sostituisci(String _ruolo, Persona _old, Persona _new)
+        {
+            switch (_ruolo.ToLower())
+            {
+				case "presidente":
+					this.Presidente = _new;
+					break;
+				case "allenatore":
+					this.Allenatore = _new;
+					break;
+				case "giocatore":
+                    for (int i = 0; i < NumeroGiocatori; i++)
+                    {
+						if (Giocatori[i].CodiceFiscale == _old.CodiceFiscale) Giocatori[i] = _new;
+                    }
+					break;
+			}
+        }
+
+		public void EliminaGioatore(Persona _persona)
+        {
+			for (int i = 0; i < NumeroGiocatori; i++)
+			{
+				if (Giocatori[i].CodiceFiscale == _persona.CodiceFiscale)
+                {
+                    for (int j = i; j < this.NumeroGiocatori - i; j++)
+                    {
+						this.Giocatori[j] = this.Giocatori[j + 1];
+                    }
+                }
+			}
+			this.Giocatori[this.NumeroGiocatori - 2] = null;
+			this.NumeroGiocatori--;
 		}
 	}
 }
